cecae55f6ab398e730c392b62ff011c2cdf36aa3
Chivos <sylvainblo@gmail.com>
2023-02-07 22:30:17 +0100

    Adding new parameters for sections

--- 00_gui.py
+++ 00_gui.py
@@ -21,26 +21,48 @@ col_parametres = [[sg.Text('Niveau RCCM'), sg.Listbox(values=('0AB', 'C', 'D'), 
                              ])]
     ]
 
-
 col_image = [[sg.Text('----', key='-CHOIXPROFILE-')],
     [sg.Image(key='-IMAGEPROFILE-')],
-    [sg.Text('Angle de rotation (degrés, sens trigo)'), sg.Input(0, key='-ANGLEROT-', size=(10,1))],
+    [sg.Text('Angle de rotation (degrés, sens trigonométrique)'), sg.Input(0, key='-ANGLEROT-', size=(10,1))],
     #[sg.Text('Choisir un fichier', size=(35, 1))],
     #[sg.InputText('Default Folder', key='folder'), sg.FolderBrowse()],
     [sg.Button('Quitter'), sg.Text(' ' * 40), sg.Button('Run')]]
 
+col_IPN = [[sg.Text('d, hauteur'), sg.Input(key='-HAUTEUR-', size=(10,1))],
+    [sg.Text('b, largeur'), sg.Input(size=(10,1))],
+    [sg.Text('t_f, épaisseur de l\'aile'), sg.Input(size=(10,1))],
+    [sg.Text('t_w, épaisseur de l\'ame'), sg.Input(size=(10,1))],
+    [sg.Text('r_r, rayon en racine'), sg.Input(size=(10,1))],
+    [sg.Text('r_f, rayon de l\'aile'), sg.Input(size=(10,1))],
+    [sg.Text('alpha, angle en degrés de l\'aile'), sg.Input(size=(10,1))],
+    [sg.Text('n_r, nombre de points de discrétisation des rayons'), sg.Input(size=(10,1))]
+    ]
 
-col_IPN = [[sg.Text('Hauteur'), sg.Input(0, key='-HAUTEUR-', size=(10,1))],
-    [sg.Text('Test IPN')]]
+col_IPE = [[sg.Text('d, hauteur'), sg.Input(key='-HAUTEUR-', size=(10,1))],
+    [sg.Text('b, largeur'), sg.Input(size=(10,1))],
+    [sg.Text('t_f, épaisseur de l\'aile'), sg.Input(size=(10,1))],
+    [sg.Text('t_w, épaisseur de l\'ame'), sg.Input(size=(10,1))],
+    [sg.Text('r, rayon en racine'), sg.Input(size=(10,1))],
+    [sg.Text('n_r, nombre de points de discrétisation du rayon'), sg.Input(size=(10,1))]
+    ]
 
-col_IPE = [[sg.Text('Hauteur'), sg.Input(0, key='-HAUTEUR-', size=(10,1))],
-    [sg.Text('Test IPE')]]
+col_UPN = [[sg.Text('d, hauteur'), sg.Input(key='-HAUTEUR-', size=(10,1))],
+    [sg.Text('b, largeur'), sg.Input(size=(10,1))],
+    [sg.Text('t_f, épaisseur de l\'aile'), sg.Input(size=(10,1))],
+    [sg.Text('t_w, épaisseur de l\'ame'), sg.Input(size=(10,1))],
+    [sg.Text('r_r, rayon en racine'), sg.Input(size=(10,1))],
+    [sg.Text('r_f, rayon de l\'aile'), sg.Input(size=(10,1))],
+    [sg.Text('alpha, angle en degrés de l\'aile'), sg.Input(size=(10,1))],
+    [sg.Text('n_r, nombre de points de discrétisation des rayons'), sg.Input(size=(10,1))]
+    ]
 
 layout = [[sg.Column(col_parametres, element_justification='c'),
         sg.VSeperator(),
         sg.Column(col_image, element_justification='c'),
-        sg.Column(col_IPN, element_justification='c', visible=False, key='col_IPN'),
-        sg.Column(col_IPE, element_justification='c', visible=False, key='col_IPE')]]
+        sg.Column(col_IPN, element_justification='r', visible=False, key='col_IPN'),
+        sg.Column(col_IPE, element_justification='r', visible=False, key='col_IPE'),
+        sg.Column(col_UPN, element_justification='r', visible=False, key='col_UPN'),
+        ]]
 
 
 
@@ -58,12 +80,16 @@ while True:
 
         window['col_IPE'].update(visible=False)
         window['col_IPN'].update(visible=False)
+        window['col_UPN'].update(visible=False)
 
         if nom_profile[0] == "IPN":
             window['col_IPN'].update(visible=True)
 
         if nom_profile[0] == "IPE":
             window['col_IPE'].update(visible=True)
+
+        if nom_profile[0] == "UPN":
+            window['col_UPN'].update(visible=True)
 
 window.close()
 
