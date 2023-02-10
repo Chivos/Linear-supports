417cee687eda15d34c5177511202974f2978b680
Chivos <sylvainblo@gmail.com>
2023-02-10 10:50:32 +0100

    Adding tooltips on input

--- 00_gui.py
+++ 00_gui.py
@@ -6,20 +6,20 @@ import solver
 sg.theme('TanBlue')
 col_parametres = [[sg.Text('Niveau RCCM'), sg.Listbox(values=('0AB', 'C', 'D'), default_values='C', size=(10, 3), key='-NIVEAU_RCCM-')],
     [sg.Text('Type profilé'), sg.Listbox(values=('IPN', 'IPE', 'UPN', 'UPE', 'Corniere', 'Rectangle'), size=(10, 6), enable_events=True, key="-PROFILE-")],
-    [sg.Text('Longueur:'), sg.Input(key='-LONGUEUR-', size=(10,1))],
-    [sg.Text('Coefficient de longueur K:'), sg.Input(2, key='-KLONGUEUR-', size=(10,1))],
-    [sg.Frame('Torseur:', [[sg.Text('N'), sg.Input(0, key='-TORSEUR_N-', size=(10,1))],
-                             [sg.Text('Fx'), sg.Input(0, key='-TORSEUR_FX-', size=(10,1))],
-                             [sg.Text('Fy'), sg.Input(0, key='-TORSEUR_FY-', size=(10,1))],
-                             [sg.Text('Mx'), sg.Input(0, key='-TORSEUR_MX-', size=(10,1))],
-                             [sg.Text('My'), sg.Input(0, key='-TORSEUR_MY-', size=(10,1))],
-                             [sg.Text('Mz'), sg.Input(0, key='-TORSEUR_MZ-', size=(10,1))]
+    [sg.Text('Longueur'), sg.Input(key='-LONGUEUR-', tooltip='mm', size=(10,1))],
+    [sg.Text('Coefficient de longueur K'), sg.Input(2, key='-KLONGUEUR-', size=(10,1))],
+    [sg.Frame('Torseur:', [[sg.Text('N'), sg.Input(0, key='-TORSEUR_N-', tooltip='N', size=(10,1))],
+                             [sg.Text('Fx'), sg.Input(0, key='-TORSEUR_FX-', tooltip='N', size=(10,1))],
+                             [sg.Text('Fy'), sg.Input(0, key='-TORSEUR_FY-', tooltip='N', size=(10,1))],
+                             [sg.Text('Mx'), sg.Input(0, key='-TORSEUR_MX-', tooltip='N.m', size=(10,1))],
+                             [sg.Text('My'), sg.Input(0, key='-TORSEUR_MY-', tooltip='N.m', size=(10,1))],
+                             [sg.Text('Mz'), sg.Input(0, key='-TORSEUR_MZ-', tooltip='N.m', size=(10,1))]
                              ],
                 element_justification='r')
     ],
-    [sg.Frame('Matériau:', [[sg.Text('Sy'), sg.Input(235, key='-SY-', size=(5,1))],
-                             [sg.Text('Su'), sg.Input(360, key='-SU-', size=(5,1))],
-                             [sg.Text('E'), sg.Input(210000, key='-MODULE-', size=(8,1))],
+    [sg.Frame('Matériau:', [[sg.Text('Sy'), sg.Input(235, key='-SY-', tooltip='MPa', size=(5,1))],
+                             [sg.Text('Su'), sg.Input(360, key='-SU-', tooltip='MPa', size=(5,1))],
+                             [sg.Text('E'), sg.Input(210000, key='-MODULE-', tooltip='MPa', size=(8,1))],
                              ],
                 element_justification='r')
     ]
@@ -27,7 +27,7 @@ col_parametres = [[sg.Text('Niveau RCCM'), sg.Listbox(values=('0AB', 'C', 'D'), 
 
 col_image = [[sg.Text('----', key='-CHOIXPROFILE-')],
     [sg.Image(key='-IMAGEPROFILE-')],
-    [sg.Text('Angle de rotation (degrés, sens trigonométrique)'), sg.Input(0, key='-ANGLEROT-', size=(5,1))],
+    [sg.Text('Angle de rotation'), sg.Input(0, key='-ANGLEROT-', tooltip='Degrés, sens trigonométrique', size=(5,1))],
     [sg.Text('Taille du maillage'), sg.Input(10, key='-MESH_SIZE-', size=(3,1))],
     #[sg.Text('Choisir un fichier', size=(35, 1))],
     #[sg.InputText('Default Folder', key='folder'), sg.FolderBrowse()],
