cd750eb162ed8f3b6d60b4a2c28c1b5ec01c109f
Chivos <sylvainblo@gmail.com>
2023-02-15 22:34:46 +0100

    Adding iter function, code rework

--- launcher.py
+++ launcher.py
@@ -3,6 +3,7 @@ import PySimpleGUI as sg
 from openpyxl import Workbook
 from openpyxl import load_workbook
 from scripts import solver
+from itertools import product
 
 
 def charger_profile(adresse):
@@ -35,20 +36,21 @@ dic_dim_UPE = charger_profile(os.path.dirname(os.path.realpath(__file__)) + "\\l
 dic_dim_COR = charger_profile(os.path.dirname(os.path.realpath(__file__)) + "\\liste_profiles\\Cornieres.xlsx")
 
 sg.theme('TanBlue')
-col_parametres = [[sg.Text('Niveau RCCM'), sg.Listbox(values=('0AB', 'C', 'D'), default_values='C', size=(10, 3), key='-NIVEAU_RCCM-')],
-    [sg.Text('Type profilé'), sg.Listbox(values=('IPN', 'IPE-HE', 'UPN', 'UPE-UAP', 'Corniere', 'Rectangle', 'Tube'), default_values='UPE', size=(10, 7), enable_events=True, key="-PROFILE-")],
+col_parametres = [[sg.Text('Niveau RCCM'), sg.Listbox(values=('0AB', 'C', 'D'), default_values='0AB', size=(10, 3), key='-NIVEAU_RCCM-')],
+    [sg.Text('Type profilé'), sg.Listbox(values=('IPN', 'IPE-HE', 'UPN', 'UPE-UAP', 'Corniere', 'Rectangle', 'Tube'), size=(10, 7), enable_events=True, key="-PROFILE-")],
     [sg.Text('Longueur'), sg.Input(key='-LONGUEUR-', tooltip='mm', size=(10,1))],
     [sg.Text('Coefficient de longueur K'), sg.Input(2, key='-KLONGUEUR-', size=(10,1))],
-    [sg.Frame('Torseur:', [[sg.Text('N'), sg.Input(0, key='-TORSEUR_N-', tooltip='N', size=(10,1))],
+    [sg.Frame('Torseur', [[sg.Text('N'), sg.Input(0, key='-TORSEUR_N-', tooltip='N', size=(10,1))],
                              [sg.Text('Fx'), sg.Input(0, key='-TORSEUR_FX-', tooltip='N', size=(10,1))],
                              [sg.Text('Fy'), sg.Input(0, key='-TORSEUR_FY-', tooltip='N', size=(10,1))],
                              [sg.Text('Mx'), sg.Input(0, key='-TORSEUR_MX-', tooltip='N.m', size=(10,1))],
                              [sg.Text('My'), sg.Input(0, key='-TORSEUR_MY-', tooltip='N.m', size=(10,1))],
-                             [sg.Text('Mz'), sg.Input(0, key='-TORSEUR_MZ-', tooltip='N.m', size=(10,1))]
+                             [sg.Text('Mz'), sg.Input(0, key='-TORSEUR_MZ-', tooltip='N.m', size=(10,1))],
+                             [sg.Checkbox('Itérer signes', key='-ITER_SIGNS-')]
                              ],
                 element_justification='r')
     ],
-    [sg.Frame('Matériau:', [[sg.Text('Sy'), sg.Input(235, key='-SY-', tooltip='MPa', size=(5,1))],
+    [sg.Frame('Matériau', [[sg.Text('Sy'), sg.Input(235, key='-SY-', tooltip='MPa', size=(5,1))],
                              [sg.Text('Su'), sg.Input(360, key='-SU-', tooltip='MPa', size=(5,1))],
                              [sg.Text('E'), sg.Input(210000, key='-MODULE-', tooltip='MPa', size=(8,1))],
                              ],
@@ -60,8 +62,6 @@ col_image = [
     [sg.Image(key='-IMAGEPROFILE-')],
     [sg.Text('Angle de rotation'), sg.Input(0, key='-ANGLEROT-', tooltip='Degrés, sens trigonométrique', size=(5,1))],
     [sg.Text('Taille du maillage'), sg.Input(10, key='-MESH_SIZE-', size=(5,1))],
-    #[sg.Text('Choisir un fichier', size=(35, 1))],
-    #[sg.InputText('Default Folder', key='folder'), sg.FolderBrowse()],
     ]
 
 ########################COLONNES MASQUEES / AFFICHEES SELON LES PROFILES###############################
@@ -98,11 +98,11 @@ col_UPN = [[sg.Combo(values=list(dic_dim_UPN), readonly=True, k='-LISTE_UPN-', s
     ]
 
 col_UPE = [[sg.Combo(values=list(dic_dim_UPE), readonly=True, k='-LISTE_UPE-', size=(20,1), enable_events=True)],
-    [sg.Text('d, hauteur'), sg.Input(80, key='In_UPE_d', size=(5,1))],
-    [sg.Text('b, largeur'), sg.Input(40, key='In_UPE_b', size=(5,1))],
-    [sg.Text('t_f, épaisseur de l\'aile'), sg.Input(5, key='In_UPE_t_f', size=(5,1))],
-    [sg.Text('t_w, épaisseur de l\'ame'), sg.Input(5, key='In_UPE_t_w', size=(5,1))],
-    [sg.Text('r, rayon en racine'), sg.Input(5, key='In_UPE_r', size=(5,1))],
+    [sg.Text('d, hauteur'), sg.Input(key='In_UPE_d', size=(5,1))],
+    [sg.Text('b, largeur'), sg.Input(key='In_UPE_b', size=(5,1))],
+    [sg.Text('t_f, épaisseur de l\'aile'), sg.Input(key='In_UPE_t_f', size=(5,1))],
+    [sg.Text('t_w, épaisseur de l\'ame'), sg.Input(key='In_UPE_t_w', size=(5,1))],
+    [sg.Text('r, rayon en racine'), sg.Input(key='In_UPE_r', size=(5,1))],
     [sg.Text('n_r, nombre de points de discrétisation du rayon'), sg.Input(15, key='In_UPE_n_r', size=(5,1))]
     ]
 
@@ -226,40 +226,69 @@ while True:
         torseur = {'N':values['-TORSEUR_N-'], 'Fx':values['-TORSEUR_FX-'], 'Fy':values['-TORSEUR_FY-'],
             'Mx':values['-TORSEUR_MX-'], 'My':values['-TORSEUR_MY-'], 'Mz':values['-TORSEUR_MZ-']}
 
+        for key, value in torseur.items(): ###Transformation string en float pour entrée dans sectionproperties
+            try:
+                torseur[key]=float(value)
+            except:
+                pass
+
         param_gene = {'niveau_RCCM':values['-NIVEAU_RCCM-'][0] ,'maille':values['-MESH_SIZE-'], 'L':values['-LONGUEUR-'], 'K':values['-KLONGUEUR-'], 'angle':values['-ANGLEROT-'],
             'Sy':values['-SY-'], 'Su':values['-SU-'], 'E':values['-MODULE-'], 'impr_res':values['-PRINT_PARAM-'], 'trac_res':values['-DRAW_RESULTS-']}
 
+        for key, value in param_gene.items():
+            try:
+                param_gene[key]=float(value)
+            except:
+                pass
+
         if nom_profile[0] == "IPN":
+            type_profile = "IH" #pour prise en compte limites de contraintes en flexion
             param_geom = {'IPN_d':values['In_IPN_d'], 'IPN_b':values['In_IPN_b'], 'IPN_t_f':values['In_IPN_t_f'], 'IPN_t_w':values['In_IPN_t_w'],
                 'IPN_r_r':values['In_IPN_r_r'], 'IPN_r_f':values['In_IPN_r_f'], 'IPN_alpha':values['In_IPN_alpha'], 'IPN_n_r':values['In_IPN_n_r']}
-            solver.calcul("IPN", param_geom, param_gene, torseur)
+            section = solver.calcul_geom("IPN", param_geom, param_gene)
 
         if nom_profile[0] == "IPE-HE":
+            type_profile ="IH"
             param_geom = {'IPE_d':values['In_IPE_d'], 'IPE_b':values['In_IPE_b'], 'IPE_t_f':values['In_IPE_t_f'],
                 'IPE_t_w':values['In_IPE_t_w'], 'IPE_r':values['In_IPE_r'], 'IPE_n_r':values['In_IPE_n_r']}
-            solver.calcul("IPE", param_geom, param_gene, torseur)
+            section = solver.calcul_geom("IPE", param_geom, param_gene)
 
         if nom_profile[0] == "UPN":
+            type_profile ="U"
             param_geom = {'UPN_d':values['In_UPN_d'], 'UPN_b':values['In_UPN_b'], 'UPN_t_f':values['In_UPN_t_f'], 'UPN_t_w':values['In_UPN_t_w'],
                 'UPN_r_r':values['In_UPN_r_r'], 'UPN_r_f':values['In_UPN_r_f'], 'UPN_alpha':values['In_UPN_alpha'], 'UPN_n_r':values['In_UPN_n_r']}
-            solver.calcul("UPN", param_geom, param_gene, torseur)
+            section = solver.calcul_geom("UPN", param_geom, param_gene)
 
         if nom_profile[0] == "UPE-UAP":
+            type_profile = "U"
             param_geom = {'UPE_d':values['In_UPE_d'], 'UPE_b':values['In_UPE_b'], 'UPE_t_f':values['In_UPE_t_f'], 'UPE_t_w':values['In_UPE_t_w'],
                 'UPE_r':values['In_UPE_r'], 'UPE_n_r':values['In_UPE_n_r']}
-            solver.calcul("UPE", param_geom, param_gene, torseur)
+            section = solver.calcul_geom("UPE", param_geom, param_gene)
 
         if nom_profile[0] == "Rectangle":
+            type_profile = "R"
             param_geom = {'REC_d':values['In_REC_d'], 'REC_b':values['In_REC_b'], 'REC_t':values['In_REC_t'], 'REC_r_out':values['In_REC_r_out'], 'REC_n_r':values['In_REC_n_r']}
-            solver.calcul("Rectangle", param_geom, param_gene, torseur)
+            section = solver.calcul_geom("Rectangle", param_geom, param_gene)
 
         if nom_profile[0] == "Corniere":
+            type_profile = "L"
             param_geom = {'COR_d':values['In_COR_d'], 'COR_b':values['In_COR_b'], 'COR_t':values['In_COR_t'], 'COR_r_r':values['In_COR_r_r'],
                 'COR_r_t':values['In_COR_r_t'], 'COR_n_r':values['In_COR_n_r']}
-            solver.calcul("Corniere", param_geom, param_gene, torseur)
+            section = solver.calcul_geom("Corniere", param_geom, param_gene)
 
         if nom_profile[0] == "Tube":
+            type_profile = "T"
             param_geom = {'TUB_d':values['In_TUB_d'], 'TUB_t':values['In_TUB_t'], 'TUB_n':values['In_TUB_n']}
-            solver.calcul("Tube", param_geom, param_gene, torseur)
+            section = solver.calcul_geom("Tube", param_geom, param_gene)
+
+        
+        if values['-ITER_SIGNS-'] == True:
+            facteurs = list(product([1, -1], repeat=6))
+        else:
+            facteurs = [[1,1,1,1,1,1]]
+
+        for i in range(len(facteurs)):
+            solver.calcul_contraintes(section, torseur, param_gene, type_profile, facteurs[i])
+
 
 window.close()
--- scripts/solver.py
+++ scripts/solver.py
@@ -1,84 +1,52 @@
-def calcul(profile, param_geom, param_gene, torseur):
-        from scripts import RCCM
-        from sectionproperties.pre.library.steel_sections import channel_section #UAP, UPE
-        from sectionproperties.pre.library.steel_sections import tapered_flange_channel #UPN
-        from sectionproperties.pre.library.steel_sections import i_section #HEA, HEB, HEC, IPE
-        from sectionproperties.pre.library.steel_sections import tapered_flange_i_section #IPN
-        from sectionproperties.pre.library.steel_sections import rectangular_hollow_section #Carré, rectangle
-        from sectionproperties.pre.library.steel_sections import angle_section #Cornière
-        from sectionproperties.pre.library.steel_sections import circular_hollow_section #Tube
-        from sectionproperties.analysis.section import Section
-        from texttable import Texttable
-        from time import time
-        import matplotlib.pyplot as plt
-        start = time()
+from scripts import RCCM
+from sectionproperties.pre.library.steel_sections import channel_section #UAP, UPE
+from sectionproperties.pre.library.steel_sections import tapered_flange_channel #UPN
+from sectionproperties.pre.library.steel_sections import i_section #HEA, HEB, HEC, IPE
+from sectionproperties.pre.library.steel_sections import tapered_flange_i_section #IPN
+from sectionproperties.pre.library.steel_sections import rectangular_hollow_section #Carré, rectangle
+from sectionproperties.pre.library.steel_sections import angle_section #Cornière
+from sectionproperties.pre.library.steel_sections import circular_hollow_section #Tube
+from sectionproperties.analysis.section import Section
+from texttable import Texttable
+import matplotlib.pyplot as plt
+
+
+def calcul_geom(profile, param_geom, param_gene):
 
         ###############################DONNEES D'ENTREE#######################################
-        
-        ###Transformation string en float pour entrée dans sectionproperties
-        for key, value in param_geom.items():
+                
+        for key, value in param_geom.items(): ###Transformation string en float pour entrée dans sectionproperties
             try:
                 param_geom[key]=float(value)
             except:
                 pass
-        for key, value in torseur.items():
-            try:
-                torseur[key]=float(value)
-            except:
-                pass
-        for key, value in param_gene.items():
-            try:
-                param_gene[key]=float(value)
-            except:
-                pass
 
         if profile == "IPN":
-                type_profile = "IH" #pour prise en compte limites de contraintes en flexion
                 geometry_0 = tapered_flange_i_section(d=param_geom['IPN_d'], b=param_geom['IPN_b'], t_f=param_geom['IPN_t_f'], t_w=param_geom['IPN_t_w'], r_r=param_geom['IPN_r_r'],
                                 r_f=param_geom['IPN_r_f'], alpha=param_geom['IPN_alpha'], n_r=int(param_geom['IPN_n_r']))
 
         if profile == "IPE": #concerne les IPE et les H (HEA, HEB, HEC...)
-                type_profile ="IH"
                 geometry_0 = i_section(d=param_geom['IPE_d'], b=param_geom['IPE_b'], t_f=param_geom['IPE_t_f'], t_w=param_geom['IPE_t_w'], r=param_geom['IPE_r'], n_r=int(param_geom['IPE_n_r']))
         
         if profile == "UPE":
-                type_profile = "U"
                 geometry_0 = channel_section(d=param_geom['UPE_d'], b=param_geom['UPE_b'], t_f=param_geom['UPE_t_f'], t_w=param_geom['UPE_t_w'], r=param_geom['UPE_r'], n_r=int(param_geom['UPE_n_r']))
         
         if profile == "UPN":
-                type_profile ="U"
                 geometry_0 = tapered_flange_channel(d=param_geom['UPN_d'], b=param_geom['UPN_b'], t_f=param_geom['UPN_t_f'],
                                 t_w=param_geom['UPN_t_w'], r_r=param_geom['UPN_r_r'], r_f=param_geom['UPN_r_f'], alpha=param_geom['UPN_alpha'], n_r=int(param_geom['UPN_n_r']))
         
         if profile == "Rectangle":
-                type_profile = "R"
                 geometry_0 = rectangular_hollow_section(d=param_geom['REC_d'], b=param_geom['REC_b'], t=param_geom['REC_t'], r_out=param_geom['REC_r_out'], n_r=int(param_geom['REC_n_r']))
 
         if profile == "Corniere":
-                type_profile = "L"
                 geometry_0 = angle_section(d=param_geom['COR_d'], b=param_geom['COR_b'], t=param_geom['COR_t'], r_r=param_geom['COR_r_r'], r_t=param_geom['COR_r_t'], n_r=int(param_geom['COR_n_r']))
 
         if profile == "Tube":
-                type_profile = "T"
                 geometry_0 = circular_hollow_section(d=param_geom['TUB_d'], t=param_geom['TUB_t'], n=int(param_geom['TUB_n']))
 
         geometry = geometry_0.rotate_section(angle=param_gene['angle']) #sens trigonométrique
         geometry.create_mesh(mesh_sizes=[param_gene['maille']])
         
-        N=torseur['N']
-        Vx=torseur['Fx']
-        Vy=torseur['Fy']
-        Mxx=torseur['Mx']*1000
-        Myy=torseur['My']*1000
-        Mzz=torseur['Mz']*1000
-
-        Sy=param_gene['Sy']
-        Su=param_gene['Su']
-        E=param_gene['E']
-
-        niveau_RCCM=param_gene['niveau_RCCM'] #niveau de contraintes, 0AB, C ou D
-        K=param_gene['K'] #conditions aux extremités
-        l=param_gene['L'] #lambda, longueur
 
         ####################################CALCULS#################################################
 
@@ -87,9 +55,31 @@ def calcul(profile, param_geom, param_gene, torseur):
 
         section.calculate_geometric_properties()
         section.calculate_warping_properties()
-        section.calculate_plastic_properties()
-       
+        section.calculate_plastic_properties() #Nécessaire ? Peut être optimisation possible
+
+        return section
+
+
+
+
+def calcul_contraintes(section, torseur, param_gene, type_profile, facteurs):       
+        
+        N=torseur['N']*facteurs[0]
+        Vx=torseur['Fx']*facteurs[1]
+        Vy=torseur['Fy']*facteurs[2]
+        Mxx=torseur['Mx']*1000*facteurs[3]
+        Myy=torseur['My']*1000*facteurs[4]
+        Mzz=torseur['Mz']*1000*facteurs[5]
+
         stress_post = section.calculate_stress(N=N, Vx=Vx, Vy=Vy, Mxx=Mxx, Myy=Myy, Mzz=Mzz)
+
+        Sy=param_gene['Sy']
+        Su=param_gene['Su']
+        E=param_gene['E']
+
+        niveau_RCCM=param_gene['niveau_RCCM'] #niveau de contraintes, 0AB, C ou D
+        K=param_gene['K'] #conditions aux extremités
+        l=param_gene['L'] #lambda, longueur
 
         r_g = min(section.get_rc()) #rayon de giration pour calcul élancement
         (ixx_c, iyy_c, ixy_c) = section.get_ic() #pour déterminer axe fort et axe faible et choix de la limite en flexion pour les poutres en I et H
@@ -100,8 +90,6 @@ def calcul(profile, param_geom, param_gene, torseur):
 
         #####################################AFFICHAGE CONTRAINTES################################################
         if bool(param_gene['trac_res']) is True:
-                #ax = geometry.plot_geometry(nrows=3, ncols=3, figsize=(12, 7), render=False)
-                #ax = section.plot_centroids(nrows=3, ncols=3, figsize=(15, 10), render=False)
                 ax = stress_post.plot_stress_n_zz(nrows=3, ncols=3, figsize=(15, 10), render=False, title="Traction liée à l'effort normal")
                 fig = ax.get_figure()
                 stress_post.plot_stress_mxx_zz(ax=fig.axes[1], title="Flexion liée au moment suivant X")
@@ -112,8 +100,6 @@ def calcul(profile, param_geom, param_gene, torseur):
                 stress_post.plot_stress_zz(ax=fig.axes[6], title="Traction tous chargements")
                 stress_post.plot_stress_zxy(ax=fig.axes[7], title="Cisaillement tous chargements")
                 section.plot_centroids(ax=fig.axes[8], title="Géométrie")
-                #ax.ticklabel_format(axis='both', style='plain') #NE MARCHE PAS !
-                #ax.set_major_formatter(ticker.StrMethodFormatter("{x:.3f}"))
                 plt.show()
 
 
@@ -137,7 +123,9 @@ def calcul(profile, param_geom, param_gene, torseur):
         (Fa, Ft, Fv, Fbx, Fby) = RCCM.criteres(Sy, Su, ixx_c, iyy_c, r_g, type_profile, niveau_RCCM, K, l, E)
 
         #########################################AFFICHAGE ET CALCUL RATIOS########################################
-                
+        
+        print('Facteurs de signes sur torseur : ', facteurs)
+
         table = Texttable()
         table.header(["", "Contrainte", "", "Limite", "Ratio", "Description"])
         
@@ -164,5 +152,3 @@ def calcul(profile, param_geom, param_gene, torseur):
         print(table.draw())
 
         ###########################################FIN##################################################################
-
-        print('\nDurée d\'exécution :', int(time() - start), 'secondes')
